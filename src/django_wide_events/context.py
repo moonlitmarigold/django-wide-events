9e7c19274a3c1bec6db00a591efbcc97b1d6104f
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-08-28 20:37:06 +0200

    feat: add event context with ContextVar
    - ContextEvent dataclass with init/get/set/drop lifecycle
    - Uses stdlib contextvars for per-request storage
    - Package __init__ imports context module

---
+++ src/django_wide_events/context.py
@@ -0,0 +1,33 @@
+from contextvars import ContextVar, Token
+import dataclasses
+
+_current: ContextVar[dict | None] = ContextVar("django_wide_events.event", default=None)
+
+@dataclasses.dataclass
+class ContextEvent:
+
+    token: Token|None = None
+
+    @staticmethod
+    def set(**kwargs):
+        ctx = ContextEvent.get()
+        ctx.update(kwargs)
+
+    @staticmethod
+    def get():
+        ctx = _current.get()
+        if ctx is None:
+            return {}
+        return ctx
+
+    def drop(self):
+        if not self.token:
+            return
+        ctx = _current.get()
+        _current.reset(self.token)
+        return ctx
+
+    @classmethod
+    def init(cls):
+        token = _current.set({})
+        return cls(token)
